4a0c58f5ff0e93976b9800aec67a821bff447d3e
Josh Purtell <jmvpurtell@gmail.com>
2024-08-19 14:23:46 -0500

    save code for both

---
+++ apropos/bench/craftax/game_information.py
@@ -0,0 +1,399 @@
+craftax_classic_game_rules = {
+    "World Information": {
+        "Actions with Requirements": {
+            "Movement": {
+                "left": {
+                    "Requirements": "Flat ground west of the agent."
+                },
+                "right": {
+                    "Requirements": "Flat ground east of the agent."
+                },
+                "up": {
+                    "Requirements": "Flat ground north of the agent."
+                },
+                "down": {
+                    "Requirements": "Flat ground south of the agent."
+                }
+            },
+            "Basic Actions": {
+                "do": {
+                    "Requirements": "Facing creature or material; have necessary tool."
+                },
+                "sleep": {
+                    "Requirements": "Energy level is below maximum."
+                },
+                "noop": {
+                    "Requirements": "Always applicable. (Means no operation.)"
+                }
+            },
+            "Crafting and Building": {
+                "place_stone": {
+                    "Requirements": "Stone in inventory."
+                },
+                "place_table": {
+                    "Requirements": "Wood in inventory."
+                },
+                "place_furnace": {
+                    "Requirements": "Stone in inventory."
+                },
+                "place_plant": {
+                    "Requirements": "Sapling in inventory."
+                },
+                "make_wood_pickaxe": {
+                    "Requirements": "Nearby table; wood in inventory."
+                },
+                "make_stone_pickaxe": {
+                    "Requirements": "Nearby table; wood, stone in inventory."
+                },
+                "make_iron_pickaxe": {
+                    "Requirements": "Nearby table, furnace; wood, coal, iron in inventory."
+                },
+                "make_wood_sword": {
+                    "Requirements": "Nearby table; wood in inventory."
+                },
+                "make_stone_sword": {
+                    "Requirements": "Nearby table; wood, stone in inventory."
+                },
+                "make_iron_sword": {
+                    "Requirements": "Nearby table, furnace; wood, coal, iron in inventory."
+                }
+            }
+        },
+        "Hierarchy of Goals": {
+            "Collect Wood": {
+                "place_table": {
+                    "make_wood_pickaxe": {
+                        "Collect Stone": {
+                            "place_stone": {},
+                            "make_stone_pickaxe": {
+                                "Collect Iron": {
+                                    "make_iron_pickaxe": {
+                                        "Collect Diamond": {},
+                                        "place_furnace": {
+                                            "make_iron_sword": {},
+                                            "make_iron_pickaxe": {}
+                                        }
+                                    }
+                                },
+                                "Collect Coal": {}
+                            }
+                        }
+                    },
+                    "make_wood_sword": {}
+                }
+            },
+            "Eat Cow": {},
+            "Collect Sapling": {
+                "place_plant": {
+                    "Eat Plant": {}
+                }
+            },
+            "Collect Drink": {},
+            "Defeat Zombie": {},
+            "Defeat Skeleton": {},
+            "Wake Up": {}
+        }
+    }
+}
+craftax_classic_action_dict = {
+    "noop": 0,
+    "left": 1,
+    "right": 2,
+    "up": 3,
+    "down": 4,
+    "do": 5,
+    "sleep": 6,
+    "place_stone": 7,
+    "place_table": 8,
+    "place_furnace": 9,
+    "place_plant": 10,
+    "make_wood_pickaxe": 11,
+    "make_stone_pickaxe": 12,
+    "make_iron_pickaxe": 13,
+    "make_wood_sword": 14,
+    "make_stone_sword": 15,
+    "make_iron_sword": 16,
+}
+
+craftax_full_game_rules = {
+"World Information": {
+    "Actions with Requirements": {
+        "Movement": {
+            "left": {
+                "Requirements": "Flat ground west of the agent."
+            },
+            "right": {
+                "Requirements": "Flat ground east of the agent."
+            },
+            "up": {
+                "Requirements": "Flat ground north of the agent."
+            },
+            "down": {
+                "Requirements": "Flat ground south of the agent."
+            }
+        },
+        "Basic Actions": {
+            "do": {
+                "Requirements": "Facing creature or material; have necessary tool."
+            },
+            "sleep": {
+                "Requirements": "Energy level is below maximum."
+            },
+            "noop": {
+                "Requirements": "Always applicable. (Means no operation.)"
+            }
+        },
+        "Crafting and Building": {
+            "place_stone": {
+                "Requirements": "Have stone in inventory; target location is empty or water."
+            },
+            "place_table": {
+                "Requirements": "Have 2 wood in inventory; target location is empty."
+            },
+            "place_furnace": {
+                "Requirements": "Have stone in inventory; target location is empty."
+            },
+            "place_torch": {
+                "Requirements": "Have torch in inventory; target location is valid for torch placement."
+            },
+            "place_plant": {
+                "Requirements": "Have sapling in inventory; target location is grass."
+            },
+            "make_wood_pickaxe": {
+                "Requirements": "At crafting table; have 1 wood in inventory."
+            },
+            "make_stone_pickaxe": {
+                "Requirements": "At crafting table; have 1 wood and 1 stone in inventory."
+            },
+            "make_iron_pickaxe": {
+                "Requirements": "At crafting table and furnace; have 1 wood, 1 stone, 1 iron, and 1 coal in inventory."
+            },
+            "make_diamond_pickaxe": {
+                "Requirements": "At crafting table; have 1 wood and 3 diamonds in inventory."
+            },
+            "make_wood_sword": {
+                "Requirements": "At crafting table; have 1 wood in inventory."
+            },
+            "make_stone_sword": {
+                "Requirements": "At crafting table; have 1 wood and 1 stone in inventory."
+            },
+            "make_iron_sword": {
+                "Requirements": "At crafting table and furnace; have 1 wood, 1 stone, 1 iron, and 1 coal in inventory."
+            },
+            "make_diamond_sword": {
+                "Requirements": "At crafting table; have 1 wood and 2 diamonds in inventory."
+            },
+            "make_iron_armour": {
+                "Requirements": "At crafting table and furnace; have 3 iron and 3 coal in inventory."
+            },
+            "make_diamond_armour": {
+                "Requirements": "At crafting table; have 3 diamonds in inventory."
+            },
+            "make_arrow": {
+                "Requirements": "At crafting table; have 1 wood and 1 stone in inventory."
+            },
+            "make_torch": {
+                "Requirements": "At crafting table; have 1 wood and 1 coal in inventory."
+            }
+        },
+        "Combat and Tools": {
+            "shoot_arrow": {
+                "Requirements": "Have bow and arrows in inventory."
+            },
+            "cast_fireball": {
+                "Requirements": "Have learned fireball spell; have at least 2 mana."
+            },
+            "cast_iceball": {
+                "Requirements": "Have learned iceball spell; have at least 2 mana."
+            },
+            "enchant_bow": {
+                "Requirements": "At enchantment table; have bow, 1 gem (ruby/sapphire), and 9 mana."
+            },
+            "enchant_sword": {
+                "Requirements": "At enchantment table; have sword, 1 gem (ruby/sapphire), and 9 mana."
+            },
+            "enchant_armour": {
+                "Requirements": "At enchantment table; have armour, 1 gem (ruby/sapphire), and 9 mana."
+            }
+        },
+        "Consumables": {
+            "drink_potion_red": {
+                "Requirements": "Have red potion in inventory."
+            },
+            "drink_potion_green": {
+                "Requirements": "Have green potion in inventory."
+            },
+            "drink_potion_blue": {
+                "Requirements": "Have blue potion in inventory."
+            },
+            "drink_potion_pink": {
+                "Requirements": "Have pink potion in inventory."
+            },
+            "drink_potion_cyan": {
+                "Requirements": "Have cyan potion in inventory."
+            },
+            "drink_potion_yellow": {
+                "Requirements": "Have yellow potion in inventory."
+            },
+            "read_book": {
+                "Requirements": "Have book in inventory."
+            }
+        },
+        "Level Navigation": {
+            "descend": {
+                "Requirements": "On down ladder; current level cleared of monsters."
+            },
+            "ascend": {
+                "Requirements": "On up ladder."
+            }
+        },
+        "Character Development": {
+            "level_up_dexterity": {
+                "Requirements": "Have at least 1 XP; dexterity below maximum."
+            },
+            "level_up_strength": {
+                "Requirements": "Have at least 1 XP; strength below maximum."
+            },
+            "level_up_intelligence": {
+                "Requirements": "Have at least 1 XP; intelligence below maximum."
+            }
+        }
+    }
+},
+    "Hierarchy of Goals": {
+        "Collect Wood": {
+            "place_table": {
+                "make_wood_pickaxe": {
+                    "Collect Stone": {
+                        "place_stone": {},
+                        "make_stone_pickaxe": {
+                            "Collect Iron": {
+                                "make_iron_pickaxe": {
+                                    "Collect Diamond": {
+                                        "make_diamond_pickaxe": {},
+                                        "make_diamond_sword": {},
+                                        "make_diamond_armour": {}
+                                    },
+                                    "place_furnace": {
+                                        "make_iron_sword": {},
+                                        "make_iron_armour": {}
+                                    }
+                                }
+                            },
+                            "Collect Coal": {}
+                        }
+                    }
+                },
+                "make_wood_sword": {}
+            }
+        },
+        "Eat Cow": {},
+        "Collect Sapling": {
+            "place_plant": {
+                "Eat Plant": {}
+            }
+        },
+        "Collect Drink": {},
+        "Defeat Zombie": {},
+        "Defeat Skeleton": {},
+        "Wake Up": {},
+        "Make Arrow": {},
+        "Make Torch": {
+            "Place Torch": {}
+        },
+        "Enter Gnomish Mines": {
+            "Defeat Gnome Warrior": {},
+            "Defeat Gnome Archer": {}
+        },
+        "Enter Dungeon": {},
+        "Enter Sewers": {},
+        "Enter Vault": {},
+        "Enter Troll Mines": {
+            "Defeat Troll": {}
+        },
+        "Enter Fire Realm": {
+            "Defeat Fire Elemental": {},
+            "Defeat Pigman": {}
+        },
+        "Enter Ice Realm": {
+            "Defeat Frost Troll": {},
+            "Defeat Ice Elemental": {}
+        },
+        "Enter Graveyard": {
+            "Damage Necromancer": {
+                "Defeat Necromancer": {}
+            }
+        },
+        "Defeat Orc Soldier": {},
+        "Defeat Orc Mage": {},
+        "Defeat Lizard": {},
+        "Defeat Kobold": {},
+        "Defeat Deep Thing": {},
+        "Eat Bat": {},
+        "Eat Snail": {},
+        "Find Bow": {
+            "Fire Bow": {}
+        },
+        "Collect Sapphire": {
+            "Learn Iceball": {
+                "Cast Iceball": {}
+            }
+        },
+        "Collect Ruby": {
+            "Learn Fireball": {
+                "Cast Fireball": {}
+            }
+        },
+        "Open Chest": {},
+        "Drink Potion": {},
+        "Enchant Sword": {},
+        "Enchant Armour": {},
+        "Defeat Knight": {},
+        "Defeat Archer": {}
+    }
+}
+craftax_full_action_dict =  {
+    "noop": 0,
+    "left": 1,
+    "right": 2,
+    "up": 3,
+    "down": 4,
+    "do": 5,
+    "sleep": 6,
+    "place_stone": 7,
+    "place_table": 8,
+    "place_furnace": 9,
+    "place_plant": 10,
+    "make_wood_pickaxe": 11,
+    "make_stone_pickaxe": 12,
+    "make_iron_pickaxe": 13,
+    "make_wood_sword": 14,
+    "make_stone_sword": 15,
+    "make_iron_sword": 16,
+    "rest": 17,
+    "descend": 18,
+    "ascend": 19,
+    "make_diamond_pickaxe": 20,
+    "make_diamond_sword": 21,
+    "make_iron_armour": 22,
+    "make_diamond_armour": 23,
+    "shoot_arrow": 24,
+    "make_arrow": 25,
+    "cast_fireball": 26,
+    "cast_iceball": 27,
+    "place_torch": 28,
+    "drink_potion_red": 29,
+    "drink_potion_green": 30,
+    "drink_potion_blue": 31,
+    "drink_potion_pink": 32,
+    "drink_potion_cyan": 33,
+    "drink_potion_yellow": 34,
+    "read_book": 35,
+    "enchant_sword": 36,
+    "enchant_armour": 37,
+    "make_torch": 38,
+    "level_up_dexterity": 39,
+    "level_up_strength": 40,
+    "level_up_intelligence": 41,
+    "enchant_bow": 42,
+}
---
+++ apropos/bench/craftax/test.py
@@ -0,0 +1,167 @@
+import asyncio
+from typing import Dict, List, Literal
+
+import numpy as np
+from pydantic import BaseModel
+
+from apropos.bench.craftax.game_dynamics import harness
+from apropos.bench.craftax.game_information import (
+    craftax_classic_action_dict,
+    craftax_classic_game_rules,
+    craftax_full_action_dict,
+    craftax_full_game_rules,
+)
+from apropos.bench.crafter.game_information import (
+    crafter_game_tips,
+)
+from apropos.src.lms.cost import CostMonitor
+from apropos.src.lms.helpers import LLM
+
+
+class ReAct(BaseModel):
+    reasoning: str
+    actions: List[str]
+
+class SimpleReActLanguageAgent:
+    obs_history: List[Dict]
+    react_history: List[Dict]
+    lm: LLM
+    cost_monitor: CostMonitor
+
+    def __init__(self, lm: LLM, cost_monitor: CostMonitor, mode: Literal["craftax_classic","craftax_full"] = "craftax_classic"):
+        self.obs_history = []
+        self.react_history = []
+        self.lm = lm
+        self.cost_monitor = cost_monitor
+        self.mode = mode
+
+    def render_history(self):
+        def remove_empty_error_section(d):
+            if "errors" in d and not len(d["errors"]["illegal_actions"])>0:
+                d.pop("errors")
+            return d
+        react_history = [f"<{i} reasoning step(s) in the past>{remove_empty_error_section(item)}</{i} reasoning step(s) in the past>" for i, item in enumerate(reversed(self.react_history[-5:]), 1)]
+        obs_history = [f"<{i} environment step(s) in the past>{remove_empty_error_section(item)}</{i} environment step(s) in the past>" for i, item in enumerate(reversed(self.obs_history[-5:]), 1)]
+        return "\n".join(react_history), "\n".join(obs_history)
+
+    async def get_actions(self):
+        if self.mode == "craftax_classic":
+            rules = craftax_classic_game_rules
+            game_tips = crafter_game_tips
+            actions = craftax_classic_action_dict
+        elif self.mode == "craftax_full":
+            rules = craftax_full_game_rules
+            game_tips = crafter_game_tips
+            actions = craftax_full_action_dict
+        else:
+            raise ValueError(f"Mode {self.mode} not recognized")
+        system_message = f"""
+# Premise
+You're playing the game of Crafter.
+Here is some information about this setting
+<Crafter Information>
+<Rules>
+{rules}
+</Rules>
+<Tips>
+{game_tips}
+</Tips>
+<Actions Available>
+{[a for a in list(actions.keys()) if a.lower() not in ['noop']]}
+</Actions Available>
+You'll be given your past actions/thoughts, along with recent raw observations from the environment
+The environment one step in the past is your current environment
+
+# Objective
+Please choose 1-3 actions to take next in the game, and provide a thought justifying the long-term end you'd like to achieve with these actions.
+These actions will be executed sequentially in the game environment, and the results will be shared with you in the next round. 
+
+# Constraints
+- If your previous actions worked toward a valuable goal, continue in that direction unless you have a good reason to change course.
+- If there's no important thought to share, don't share anything. "No update" is a valid and the most common response.
+- Render your actions by name. All rendered names must match the names in the "Actions Available" section.
+- If you attempt an action that requires certain conditions to be met without meeting those conditions, the action will fail.
+- Never select 'Do' multiple times in a row unless fighting a mob or enemy. In other cases, it's a wasted action. 
+    When you select 'Do', you will interact with whatever is one step in the direction you are facing (indicated under the Object_you_are_facing heading if there is anything) and zero steps away in every other direction. If there is nothing one step (e.g. one step up, one step down, one step right, or one step left) in the direction you are facing, your 'Do' action will fail.
+        - If something is e.g. one step up and one step right, attempting to interact with it will fail. You must be only one step away in the direction you are facing.
+- To change the direction you are facing, move in the direction you want to face.
+"""
+        react_history, obs_history = self.render_history()
+        user_message = f"""
+# Recent Actions / Thoughts
+{react_history}
+# Recent Observations
+{obs_history}
+
+Your next actions / thought: """
+        
+        react_step = await self.lm.async_respond(
+            system_prompt = system_message,
+            user_prompt = user_message,
+            response_model = ReAct,
+        )
+        
+        illegal_actions = [action for action in react_step.actions if action not in craftax_classic_action_dict.keys()]
+        legal_actions = [action for action in react_step.actions if action in craftax_classic_action_dict.keys()]
+        react_info = react_step.dict()
+        react_info["errors"] = {
+            "illegal_actions": illegal_actions,
+        }
+        
+        self.react_history.append(react_info)
+        self.cost_monitor.update_token_counts(system_message, user_message, str(react_step.dict()))
+        
+        return legal_actions
+
+    async def add_observations(self, observations: List[Dict]):
+        for observation in observations:
+            self.obs_history.append(observation["state"])#[0].default_readout()
+
+def hafner_scoring_function_modified(achievements_probs: Dict):
+    probs = list(achievements_probs.values())
+    return sum([np.log(1 + prob) for prob in probs])
+
+# Log token count to get approx cost of rollout.
+async def score_rollouts(k_steps=10, n_rollouts=10, verbose=False, model_name="gpt-4o-mini", modality: Literal["text","vision"]="text", mode: Literal["craftax_classic","craftax_full"]="craftax_classic"):
+    core_lm = LLM(model_name)
+    cost_monitor = CostMonitor(
+        model_name=core_lm.model_name
+    )
+    if modality == "text":
+        agent = SimpleReActLanguageAgent(lm=core_lm, cost_monitor=cost_monitor, mode=mode)
+    elif modality == "vision":
+        raise NotImplementedError("Vision not implemented yet")
+    async def single_rollout(seed):
+        try:
+            achievements_log = await harness(agent, seed=seed, k_steps=k_steps, verbose=verbose, mode=mode)
+        except Exception as e:
+            print(f"Error in Rollout: {e}")
+            return
+        achievements = list({k: v for k, v in achievements_log.items() if v > 0}.keys())
+        print("Achievements", achievements)
+        action_density = len(agent.obs_history) / len(agent.react_history)
+        price, n_tokens = cost_monitor.final_cost()
+        if verbose and action_density < 2:
+            print(f"Warning - Low Action Density of {action_density}")
+        if verbose:
+            print(f"Rollout price ${price:.2f} for {n_tokens} tokens")
+        return achievements, price
+    achivements_and_price_by_rollout = await asyncio.gather(*[single_rollout(seed=_) for _ in range(n_rollouts)])
+    achivements_and_price_by_rollout = [a_and_p for a_and_p in achivements_and_price_by_rollout if a_and_p is not None]
+    total_price = sum([price for _, price in achivements_and_price_by_rollout])
+    achievements_probs = {k: len([achievements for achievements, _ in achivements_and_price_by_rollout if k in achievements]) / len(achivements_and_price_by_rollout) for k in set([achievement for achievements, _ in achivements_and_price_by_rollout for achievement in achievements])}
+    return hafner_scoring_function_modified(achievements_probs), achievements_probs, total_price
+
+if __name__ == "__main__":
+    #gpt-4o-2024-08-06
+    #gpt-4o-mini-2024-07-18
+    model_name = "gpt-4o-mini-2024-07-18"
+    mode = "craftax_full"
+    hafner_score,achievement_probs, total_price = asyncio.run(
+        score_rollouts(k_steps=50, n_rollouts=1, verbose=False, model_name=model_name,modality="text", mode =mode)#hermes-3-llama-3.1-405b-fp8-128k
+    )
+    print("Agent got a normalized Crafter score of", hafner_score)
+    print("Achievement Probabilities:")
+    for k, v in achievement_probs.items():
+        print(f"{k}: {v:.3f}")
+    print(f"Total price of experiment: ${total_price:.2f}")#TODO: add a way to price images
